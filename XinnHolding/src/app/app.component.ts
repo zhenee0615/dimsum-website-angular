cd2b54f0a02f9f5fa71f09f872251c49120d60ce
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-12 12:24:48 +0800

    backup

--- XinnHolding/src/app/app.component.ts
+++ XinnHolding/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Version } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'XinnHolding';
+  title = 'Xinn Holding';
+  images: string[] = ["/assets/Images/password_hide.png", "/assets/Images/password_reveal.png"];
+  image :string = this.images[0];
+
+  viewPW() {
+    this.image = (this.image == this.images[0]) ? this.images[1] : this.images[0];
+  }
 }
+
+

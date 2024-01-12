abff17447586a9043975f1e166fc27efb21dc467
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-12 18:31:15 +0800

    login page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { Component, Version } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { LoginModule } from './login/login.module';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  selector: 'app-root',
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Xinn Holding';
-  images: string[] = ["/assets/Images/password_hide.png", "/assets/Images/password_reveal.png"];
-  image :string = this.images[0];
-
-  viewPW() {
-    this.image = (this.image == this.images[0]) ? this.images[1] : this.images[0];
-  }
+  
 }
-
-

b45e7c4aeae33c0f3476aa909e75a7f3995efb43
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-15 14:53:40 +0800

    resolve header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   
 }

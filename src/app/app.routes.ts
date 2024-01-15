10b61840155a29d09e9a4880bf2474b2bd212df3
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-15 17:19:56 +0800

    insert icons with ngfor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: "/home", pathMatch: 'full'},
-    {path: 'login', component: LoginComponent},
-    {path: 'home', component: HomeComponent}
+    {path: '', redirectTo: "/home", pathMatch: 'full', title: 'Home'},
+    {path: 'login', component: LoginComponent, title: 'Login'},
+    {path: 'home', component: HomeComponent, title: 'Home'}
 ];
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 
 export class FooterComponent {
-
+  iconSrc = ["assets/Images/fb_icon.png", 
+            "assets/Images/insta_icon.png", 
+            "assets/Images/whatsapp_icon.png", 
+            "assets/Images/youtube_icon.png"];
 }

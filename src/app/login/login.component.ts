856e1a94ef6ee69ba722891bb7afd58fe1a877e8
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-15 11:46:48 +0800

    make changes for correction

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   images: string[] = ["/assets/Images/password_hide.png", "/assets/Images/password_reveal.png"];
-  image :string = this.images[0];
+  image : string = this.images[0];
 
   viewPW() {
     this.image = (this.image == this.images[0]) ? this.images[1] : this.images[0];

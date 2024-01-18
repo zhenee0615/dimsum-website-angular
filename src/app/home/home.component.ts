14bd87499f2fb36a67e80844da4af780b5ea09f7
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-18 16:01:49 +0800

    banner done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, { SwiperOptions, Autoplay, Navigation, Pagination} from 'swiper';
+
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SwiperModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 
 export class HomeComponent {
+  images = [
+    { id: 1, path: "assets/Images/cnyOfferBanner.jpg" },
+    { id: 2, path: "assets/Images/dimsumBanner.jpg" },
+    { id: 3, path: "assets/Images/freeshipping.png" }
+  ]
 
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    navigation: false,
+    autoplay: { delay: 1500, disableOnInteraction: false },
+    pagination: { 
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+      type: 'bullets',
+    }, 
+    loop: true
+  };
 }

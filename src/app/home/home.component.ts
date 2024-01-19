47e040663d802074b136e16474ea1af9908134d5
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-19 16:24:33 +0800

    product list done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { config } from '../constant';
+import { bannerSwiper } from '../constant';
 import { SwiperModule } from 'swiper/angular';
 import SwiperCore, { SwiperOptions, Autoplay, Navigation, Pagination} from 'swiper';
+import { WeeklyFlashSalesComponent } from '../weekly-flash-sales/weekly-flash-sales.component';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SwiperModule],
+  imports: [CommonModule, SwiperModule, WeeklyFlashSalesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 
 export class HomeComponent {
-  images = [
+  bannerImages = [
     { id: 1, path: "assets/Images/cnyOfferBanner.jpg" },
     { id: 2, path: "assets/Images/dimsumBanner.jpg" },
     { id: 3, path: "assets/Images/freeshipping.png" }
-  ]
+  ];
 
-  config: SwiperOptions = config;
+  bannerSwiper: SwiperOptions = bannerSwiper;
 }

910a71a2ee8682f2252c66cebd113d5e48b66a84
Toh Zhen Ee <zhenee.toh@agmo.group>
2024-01-22 10:15:35 +0800

    formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { WeeklyFlashSalesComponent } from './weekly-flash-sales/weekly-flash-sales.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
 export const routes: Routes = [
